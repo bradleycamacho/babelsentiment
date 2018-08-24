ed1cce3fcd57c179b1a24b8b432d716ec374acc5
Brad <bradleyrcamacho98@gmail.com>
2018-08-24 13:30:09 -0400

    Uploaded all files

---
+++ IrbyNrc.R
@@ -0,0 +1,27 @@
+library(syuzhet)
+
+##tokenizes input and stores sentiment in vectors
+input <- get_text_as_string("Texts/Irby.txt")
+iSent <- get_sentences(input)
+
+nrcInput <- get_nrc_sentiment(iSent)
+
+png("Plots/nrc/Irby/total.png", width = 1200,height = 700)
+barplot(
+  sort(colSums(prop.table(nrcInput[, 1:8]))), 
+  horiz = TRUE, 
+  cex.names = 0.7, 
+  las = 1, 
+  main = "Emotions in Irby", xlab="Percentage"
+)
+dev.off()
+
+png("Plots/nrc/Irby/Sent.png", width = 1200,height = 700)
+barplot(
+  sort(colSums(prop.table(nrcInput[, 9:10]))), 
+  horiz = TRUE, 
+  cex.names = 0.7, 
+  las = 1, 
+  main = "Sentiment in Irby", xlab="Percentage"
+)
+dev.off()
